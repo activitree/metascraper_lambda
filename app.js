239f50f185dfa294f62c5e0d05f61cdc1bf2dc64
paulincai <paulailincai@icloud.com>
2019-07-13 20:45:49 +0400

    Libraries updates and removed validatios from Lamda
    Validations were being done anyway on the client side.

--- app.js
+++ app.js
@@ -5,20 +5,29 @@ const metascraper = require('metascraper')([
 	require('metascraper-description')(),
 	require('metascraper-image')(),
 	require('metascraper-logo')(),
-	require('metascraper-clearbit-logo')(),
+	require('metascraper-clearbit')({
+		size: 256,
+		format: 'jpg'
+	}),
 	require('metascraper-publisher')(),
 	require('metascraper-title')(),
-	require('metascraper-url')()
+	require('metascraper-url')(),
+	require('metascraper-soundcloud')(),
+	require('metascraper-video')(),
+	require('metascraper-youtube')()
 ])
 const got = require('got')
 const api = new ApiBuilder()
 
 api.post('/metascraper', async req => {
+
+	// let targetUrl = req.queryString.url
+
+	/*
   if (!req.hasOwnProperty('queryString') || !req.queryString.hasOwnProperty('url')) {
     throw new Error('Invalid request. url parameter missing')
   }
   // Check if it's a somewhat valid URL
-  let targetUrl = req.queryString.url
 
   if (!/^(http|https):\/\/[^ ']+$/.test(targetUrl)) {
     // Check if maybe only the http part is missing?
@@ -27,10 +36,10 @@ api.post('/metascraper', async req => {
     if (!/^(http|https):\/\/[^ ']+$/.test(targetUrl)) {
       throw new Error('Invalid request. url invalid or non-HTTP(S)')
     }
-  }
+  }*/
 
 	try {
-		const { body: html, url } = await got(targetUrl)
+		const { body: html, url } = await got(req.queryString.url)
 
 		return await metascraper({url, html})
 
